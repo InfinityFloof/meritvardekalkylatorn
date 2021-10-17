fb205a3e2f8f78bdc2e559369147e1d2a845e70a
Alex S <alex.serrano.rooth@protonmail.com>
2021-10-17 20:26:53 +0200

    Finished

--- studyhero.go
+++ studyhero.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 var (
-	amnen = []string{"Bild", "Biologi", "Engelska", "Fysik", "Geografi", "Hemkunskap", "Historia", "Idrott", "Kemi", "Matematik", "Moderna Språk", "Musik", "Religionkunskap", "Samhällskunskap", "Slöjd", "Svenska", "Teknik"}
-	betyg = make(map[string]float32)
-	svar  string
-	total float32
+	personbetyg     = make(map[string]float32)
+	steg            = make(map[string]float32)
+	amnen           = []string{"Bild", "Biologi", "Engelska", "Fysik", "Geografi", "Hemkunskap", "Historia", "Idrott", "Kemi", "Matematik", "Moderna Språk", "Musik", "Religionkunskap", "Samhällskunskap", "Slöjd", "Svenska", "Teknik"}
+	ezamnen         = []string{"Biologi", "Engelska", "Fysik", "Geografi", "Historia", "Kemi", "Matematik", "Moderna Språk", "Religionkunskap", "Samhällskunskap", "Slöjd", "Svenska", "Teknik"}
+	hdamnen         = []string{"Bild", "Hemkunskap", "Idrott", "Musik"}
+	betyg           = make(map[string]float32)
+	svar            string
+	metod           int
+	wishmerit       float32
+	differencemerit float32
+	totalmerit      float32
 )
 
 func main() {
@@ -20,7 +27,100 @@ func main() {
 	for i := 0; i < cap(amnen); i++ {
 		fmt.Printf("%v betyg: ", amnen[i])
 		fmt.Scanf("%v", &svar)
-		total += betyg[svar]
+		personbetyg[amnen[i]] = betyg[svar]
+		totalmerit += betyg[svar]
 	}
-	fmt.Printf("Ditt meritvärde är: %v\nKämpa på!\n", total)
+	fmt.Printf("Ditt meritvärde är: %v\n", totalmerit)
+	fmt.Printf("Om du inte når dina mål så är det inte kört. Vad är ditt önskade meritvärde?: ")
+	fmt.Scanf("%v", &wishmerit)
+	differencemerit = wishmerit - totalmerit
+	fmt.Printf("För att få ditt önskade meritvärde måste vi uppnå %v poäng till\n", differencemerit)
+	fmt.Printf("Det finns flera sätt vi kan lösa det här på\n")
+	fmt.Printf("1: Plugga flera ämnen men inte behöva höja mycket i dem\n")
+	fmt.Printf("2: Plugge färre ämnen men behöva uppnå högt i dem\n")
+	fmt.Printf("Vad vill du göra?: ")
+	fmt.Scanf("%v", &metod)
+	switch metod {
+	case 1:
+		// Försök 1, lätta betyg
+		for i := 0; i < cap(ezamnen); i++ {
+			if differencemerit == 0 {
+				break
+			}
+			if personbetyg[ezamnen[i]] != 20 {
+				personbetyg[ezamnen[i]] += 2.5
+				steg[ezamnen[i]] += 1
+			}
+		}
+		// Försök 2, lätta betyg
+		for i := 0; i < cap(ezamnen); i++ {
+			if differencemerit == 0 {
+				break
+			}
+			if personbetyg[ezamnen[i]] != 20 {
+				personbetyg[ezamnen[i]] += 2.5
+				steg[ezamnen[i]] += 1
+			}
+		}
+		// Försök 3, lätta betyg
+		for i := 0; i < cap(ezamnen); i++ {
+			if differencemerit == 0 {
+				break
+			}
+			if personbetyg[ezamnen[i]] != 20 {
+				personbetyg[ezamnen[i]] += 2.5
+				steg[ezamnen[i]] += 1
+			}
+		}
+		// Försök 3, svåra betyg
+		for i := 0; i < cap(hdamnen); i++ {
+			if differencemerit == 0 {
+				break
+			}
+			if personbetyg[hdamnen[i]] != 20 {
+				personbetyg[hdamnen[i]] += 2.5
+				steg[hdamnen[i]] += 1
+			}
+		}
+		// Försök 4, svåra betyg
+		for i := 0; i < cap(hdamnen); i++ {
+			if differencemerit == 0 {
+				break
+			}
+			if personbetyg[hdamnen[i]] != 20 {
+				personbetyg[hdamnen[i]] += 2.5
+				steg[hdamnen[i]] += 1
+			}
+		}
+	case 2:
+		// Försök 1, Lätta betyg
+		for x := 0; x <= 10; x++ {
+			for i := 0; i < cap(ezamnen); i++ {
+				if differencemerit == 0 {
+					break
+				}
+				if personbetyg[ezamnen[i]] <= 17.5 {
+					personbetyg[ezamnen[i]] += 2.5
+					steg[ezamnen[i]] += 1
+				}
+			}
+		}
+		// Försök 2, Svåra betyg
+		for x := 0; x <= 10; x++ {
+			for i := 0; i < cap(hdamnen); i++ {
+				if differencemerit == 0 {
+					break
+				}
+				if personbetyg[hdamnen[i]] <= 17.5 {
+					personbetyg[hdamnen[i]] += 2.5
+					steg[hdamnen[i]] += 1
+				}
+			}
+		}
+	}
+	fmt.Printf("Du behöver öka i (Steg):\n")
+	for i := 0; i < cap(amnen); i++ {
+		fmt.Printf("%v: %v steg\n", amnen[i], steg[amnen[i]])
+	}
+
 }
