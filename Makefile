37e90def07098a960b09db6feb03063240930bb2
Alex S <75302078+InfinityFloof@users.noreply.github.com>
2021-10-17 18:55:07 +0200

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,4 @@
+ meritkalc:
+	go build meritkalc.go
+install:
+	sudo mv meritkalc /usr/bin/meritkalc
---
+++ meritkalc.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var (
+	amnen = []string{"Bild", "Biologi", "Engelska", "Fysik", "Geografi", "Hemkunskap", "Historia", "Idrott", "Kemi", "Matematik", "Moderna Språk", "Musik", "Religionkunskap", "Slöjd", "Svenska", "Teknik"}
+	betyg = make(map[string]float32)
+	svar  string
+	total float32
+)
+
+func main() {
+	betyg["A"] = 20
+	betyg["B"] = 17.5
+	betyg["C"] = 15
+	betyg["D"] = 12.5
+	betyg["E"] = 10
+	betyg["F"] = 0
+	fmt.Printf("OBS: Stora bokstäver!\n")
+	for i := 0; i < cap(amnen); i++ {
+		fmt.Printf("Skriv ditt %v betyg: ", amnen[i])
+		fmt.Scanf("%v", &svar)
+		total += betyg[svar]
+	}
+	fmt.Printf("Ditt meritvärde är: %v\nKämpa på!\n", total)
+}
